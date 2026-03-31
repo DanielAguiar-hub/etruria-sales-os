e95751d0adfbecb7e487f74758171540246efdba
Daniel Aguiar <da868224@gmail.com>
2026-03-31 16:40:45 -0300

    🔧 Corrigindo links do PWA (404 no celular)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'etruria-crm-v1';
 const ASSETS = [
-  './crm_ecommerce.html',
+  './index.html',
   './manifest.json',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
